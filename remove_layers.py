88dd05b9397e884d8f12c75642bb1a05b5ce84fa
TimHeiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2018-02-25 19:55:05 +0100

    UPDATE remove_layers
    Updated remove layer

--- remove_layers.py
+++ remove_layers.py
@@ -3,12 +3,19 @@ def remove_layers(data):
     Removes all unneeded layers of dictionaries, lists and tuples
     """
     while True:
-        if type(data)==int or type(data)==float or len(data)!=1:
+        isdictlisttuple=type(data)==dict or type(data)==list or type(data)==tuple
+        
+        if type(data)==int or type(data)==float:
             break
+        elif isdictlisttuple:
+            if len(data)!=1:
+                break
         
         if type(data)==dict:
             data=data[list(data.keys())[0]]
-        else:
+        elif isdictlisttuple and type(data)!=dict:
             data=data[0]
+        else:
+            break
     
     return data
